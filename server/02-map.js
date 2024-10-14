4b0cfcfd5c1fd36f6bd05f0ec8d6a82ffd6aa9eb
OrlandoGonzales13 <orlando13gonzales@gmail.com>
2024-10-14 16:08:42 -0500

    Map

---
+++ server/02-map.js
@@ -0,0 +1,13 @@
+const letters = [`a`, `b`, `c`, `d`]
+
+const newArray = letters.map(item => item + `++`)
+
+// const newArray = [];
+
+// for (let index = 0; index < letters.length; index++) {
+//     const element = letters[index];
+//     newArray.push(element + "++");
+// }
+
+console.log(`Original -> ` + letters)
+console.log(`New -> ` + newArray)
